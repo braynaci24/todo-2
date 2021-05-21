827adf75512e229480939bee47bcbebc61f00249
braynaci24 <aynacimuhammetemin@gmail.com>
2021-05-21 15:14:31 +0300

    general: style, js, html

--- js/main.js
+++ js/main.js
@@ -6,12 +6,15 @@ $(document).ready(function () {
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min);
     }
-    function reload (){
+
+    function reload() {
         window.location.reload()
     }
+    
     $('.send-button').click(function () {
+        let nameListContainer = $('.name-list')
         let nameInfo = $('.todo-name-input').val();
-        if (nameInfo.length > 0) {
+        if (nameInfo.length > 0 ) {
             outGoingName.push(nameInfo)
             $('.name-list').append(`<div class="list-element-general-box"><li class="list-element">${nameInfo}</li><a href="#"><i class="far fa-trash-alt trash"></i></a></div>`)
             $('.todo-name-input').val("").focus();
@@ -23,15 +26,16 @@ $(document).ready(function () {
         } else {
             Swal.fire({
                 icon: 'error',
-                title: 'Önce kişileri ve ne yapılacağını gir',
+                title: 'ÖNCE KİŞİLERİ VE NE YAPILACAĞINI GİR!',
             })
         }
     })
 
     $('.start').click(function () {
         let mission = $('.todo-info-input').val();
+        let RandomName = outGoingName[getRandomInt(0, outGoingName.length)];
         if (mission.length > 3) {
-            $('.paragraph-container').append(`<p class="mission-paragraph">${mission} görevi ${outGoingName[getRandomInt(0, outGoingName.length)]} kişisine kitlendi.</p>`)
+            $('.paragraph-container').append(`<p class="mission-paragraph">${mission} görevi ${RandomName} kişisine kitlendi.</p>`)
             $(this).hide();
         } else {
             Swal.fire({
@@ -42,17 +46,20 @@ $(document).ready(function () {
         $('.send-button').hide();
         $('.refresh-button').show();
     })
+
     $('.refresh-button').click(function () {
         reload();
     })
+
     $('body').on('click', '.trash', function () {
         $(this).parent().parent().remove();
-        if($('.list-element-general-box').length < 1){
+        if ($('.list-element-general-box').length < 1) {
             $('.start').remove();
             reload();
         }
     })
-    $('body').on('click', '.list-element', function(){
+
+    $('body').on('click', '.list-element', function () {
         $(this).parent().addClass('line-through')
     })
 
