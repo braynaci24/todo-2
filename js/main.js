7af78eab1ad7f3b6c835c239b8f73c485546992b
braynaci24 <aynacimuhammetemin@gmail.com>
2021-05-21 14:12:20 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
     let outGoingName = []
+
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min);
     }
-
+    function reload (){
+        window.location.reload()
+    }
     $('.send-button').click(function () {
         let nameInfo = $('.todo-name-input').val();
         if (nameInfo.length > 0) {
             outGoingName.push(nameInfo)
-            $('.name-list').append(`<li class="element">${nameInfo}</li>`)
+            $('.name-list').append(`<div class="list-element-general-box"><li class="list-element">${nameInfo}</li><a href="#"><i class="far fa-trash-alt trash"></i></a></div>`)
             $('.todo-name-input').val("").focus();
 
             if (outGoingName.length > 1) {
@@ -28,8 +31,8 @@ $(document).ready(function () {
     $('.start').click(function () {
         let mission = $('.todo-info-input').val();
         if (mission.length > 3) {
-            $('.paragraph').append(`<p class="mission-paragraph">${mission} görevi ${outGoingName[getRandomInt(0, outGoingName.length)]} kişisine kitlendi.</p>`)
-            $(this).fadeOut(500);
+            $('.paragraph-container').append(`<p class="mission-paragraph">${mission} görevi ${outGoingName[getRandomInt(0, outGoingName.length)]} kişisine kitlendi.</p>`)
+            $(this).hide();
         } else {
             Swal.fire({
                 icon: 'error',
@@ -39,8 +42,18 @@ $(document).ready(function () {
         $('.send-button').hide();
         $('.refresh-button').show();
     })
-    $('.refresh-button').click(function(){
-        window.location.reload()
+    $('.refresh-button').click(function () {
+        reload();
+    })
+    $('body').on('click', '.trash', function () {
+        $(this).parent().parent().remove();
+        if($('.list-element-general-box').length < 1){
+            $('.start').remove();
+            reload();
+        }
+    })
+    $('body').on('click', '.list-element', function(){
+        $(this).parent().addClass('line-through')
     })
 
 })
